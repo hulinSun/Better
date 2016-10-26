c598e7b8f56980293da182031d624450ea8ceaf0
Hony <438661443@qq.com>
2016-10-26 16:16:02 +0800

    fix

--- better/better/Classes/Home/Controller/HomeViewController.swift
+++ better/better/Classes/Home/Controller/HomeViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireObjectMapper
 
 class HomeViewController: UIViewController {
 
@@ -59,9 +61,33 @@ class HomeViewController: UIViewController {
             })
         }
         
-        HomeHttpHelper.requestHomeData { (response) in
-            print(response)
+        
+        let parame = [
+            "app_id": "com.jzyd.Better",
+            "app_installtime": "1476251286",
+            "app_versions": "1.5.1",
+            "channel_name": "appStore",
+            "client_id": "bt_app_ios",
+            "client_secret": "9c1e6634ce1c5098e056628cd66a17a5",
+            "device_token": "711214f0edd8fe4444aa69d56119e0bbf83bc1675292e4b9e81b0a83a7cdff0a",
+            "last_get_time": "1476607050",
+            "oauth_token": "1cfdf7dc28066c076c23269874460b58",
+            "os_versions": "10.0.2",
+            "page": "0",
+            "pagesize" : "20",
+            "screensize": "750",
+            "track_device_info": "iPhone7%2C2",
+            "track_deviceid": "18B31DD0-2B1E-49A9-A78A-763C77FD65BD",
+            "track_user_id": "2670024",
+            "v": "14"]
+        
+        Alamofire.request("http://open3.bantangapp.com/recommend/index", method: .get, parameters: parame).responseObject { (response: DataResponse<HomePageModel>) in
+            let homedata = response.result.value
+            if let topic = homedata?.data?.topic?.first{
+                print(topic.title)
+            }
         }
+
     }
     
     override func didReceiveMemoryWarning() {
