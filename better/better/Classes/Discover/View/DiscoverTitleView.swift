7f0b0f3bf2739f1438754a9666180c19f49039ef
Hony <438661443@qq.com>
2016-11-01 16:27:17 +0800

    fox

--- better/better/Classes/Discover/View/DiscoverTitleView.swift
+++ better/better/Classes/Discover/View/DiscoverTitleView.swift
@@ -14,4 +14,39 @@ class DiscoverTitleView: UIView {
     return Bundle.main.loadNibNamed("DiscoverTitleView", owner: nil, options: nil)?.last as! DiscoverTitleView
     }
     
+    enum ItemSelected {
+        case left, right
+    }
+    
+    typealias clickItemClosure = (_ itemSel: ItemSelected) ->Void
+    
+    
+    var clickItemback: clickItemClosure?
+    
+    @IBOutlet weak var botLine: UIView!
+    @IBOutlet weak var leftBtn: UIButton!
+    @IBOutlet weak var rightBtn: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        leftBtn.isSelected = true
+    }
+    
+    
+    /// 点击左边按钮
+    @IBAction func leftClick(_ sender: AnyObject) {
+        leftBtn.isSelected = true
+        rightBtn.isSelected = false
+        clickItemback?(ItemSelected.left)
+    }
+    
+    /// 点击右边按钮
+    @IBAction func rightClick(_ sender: AnyObject) {
+        leftBtn.isSelected = false
+        rightBtn.isSelected = true
+        clickItemback?(ItemSelected.right)
+    }
+    
+    
+    
 }
--- better/better/Classes/Main/config/Theme.swift
+++ better/better/Classes/Main/config/Theme.swift
@@ -10,8 +10,10 @@ import UIKit
 
 
 struct UIConst {
+    
     /// 主题色
-    static let themeColor = UIColor.rgb(red: 250, green: 100, blue: 102)
+    static let themeColor = UIColor.init(hexString: "ec5252")
+    
     /// 首页pagecontroll 的黄色
     static let yellowPageColor = UIColor.rgb(red: 254, green: 239, blue: 57)
     /// 首页pagecontroll 的灰色
