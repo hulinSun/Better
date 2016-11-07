a974954b5966c3ff0eaf51b53493cb77744084de
Hony <438661443@qq.com>
2016-11-07 16:53:33 +0800

    fix

---
+++ better/better/Classes/Discover/View/HotRecommondCell.swift
@@ -0,0 +1,28 @@
+//
+//  HotRecommondCell.swift
+//  better
+//
+//  Created by Hony on 2016/11/7.
+//  Copyright © 2016年 Hony. All rights reserved.
+//
+
+import UIKit
+
+class HotRecommondCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        
+        
+        
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+    
+}
