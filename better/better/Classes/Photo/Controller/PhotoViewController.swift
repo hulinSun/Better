ff42269a6edbdf8720e9d5e35c03bd1b6936a9c5
Hony <438661443@qq.com>
2016-11-10 15:06:38 +0800

    获取图片

--- better/better/Classes/Photo/Controller/PhotoViewController.swift
+++ better/better/Classes/Photo/Controller/PhotoViewController.swift
@@ -7,18 +7,39 @@
 //
 
 import UIKit
+import Photos
 
 class PhotoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
-        // Do any additional setup after loading the view.
+        
+        getCollection()
+        
+    }
+    
+    func getCollection() {
+        
+        let opt = PHImageRequestOptions()
+        opt.isSynchronous = true
+        let collects =  PHAssetCollection.fetchAssetCollections(with: .album, subtype: .albumRegular, options: nil)
+        /// 这里为什么不能用尾闭包的写法/。否则会报错
+        collects.enumerateObjects ({ (collection, start, stop) in
+            // 相册--> 图片
+            let imageColls = PHAsset.fetchAssets(in: collection, options: nil)
+            imageColls.enumerateObjects({ (asset, start, stop) in
+                print(asset)
+                
+                PHImageManager.default().requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFill, options: nil, resultHandler: { (image, dict) in
+                    print(image)
+                })
+            })
+        })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     
