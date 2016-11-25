334e13ef30de6e28b0c9f2fa477bc39f38cedd52
Hony <438661443@qq.com>
2016-11-25 14:37:07 +0800

    添加滤镜库

--- better/better/AppDelegate.swift
+++ better/better/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GPUImage
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
         /// 初始化window
         setupWindow()
         return true
