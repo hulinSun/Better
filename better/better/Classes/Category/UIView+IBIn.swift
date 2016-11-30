a0414717b6fa0c2ae8f1635bf13f8755de0a9b8f
Hony <438661443@qq.com>
2016-11-30 11:37:19 +0800

    fix

---
+++ better/better/Classes/Category/UIView+IBIn.swift
@@ -0,0 +1,29 @@
+//
+//  UIView+IBIn.swift
+//  better
+//
+//  Created by Hony on 2016/11/30.
+//  Copyright © 2016年 Hony. All rights reserved.
+//
+
+import UIKit
+
+//public protocol UIViewXIBProtocol{
+//    var cornerRadius: CGFloat{get set}
+//}
+//
+//
+//extension UIView : UIViewXIBProtocol {}
+//
+//public extension UIViewXIBProtocol where Self : UIView {
+////    @IBInspectable
+//    var cornerRadius: CGFloat{
+//        set{
+//            self.layer.cornerRadius = cornerRadius
+//            self.clipsToBounds = true
+//        }
+//        get{
+//            return self.layer.cornerRadius
+//        }
+//    }
+//}
--- better/better/Classes/Photo/View/PhotoFilterCell.swift
+++ better/better/Classes/Photo/View/PhotoFilterCell.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class PhotoFilterCell: UICollectionViewCell {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var iconView: UIImageView!{
+        didSet{
+            iconView.layer.cornerRadius = 2
+            iconView.clipsToBounds = true
+            iconView.backgroundColor = UIColor.random()
+        }
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
