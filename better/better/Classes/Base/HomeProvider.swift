bff559aaf8c96b7441619852507837997996e9bb
Hony <438661443@qq.com>
2016-12-20 09:47:52 +0800

    fix

---
+++ better/better/Classes/Base/HomeProvider.swift
@@ -0,0 +1,22 @@
+//
+//  HomeProvider.swift
+//  better
+//
+//  Created by Hony on 2016/12/20.
+//  Copyright © 2016年 Hony. All rights reserved.
+//
+
+import UIKit
+
+enum HomeService {
+    case home
+    case showTopics(id: Int)
+}
+
+extension HomeService{
+    
+}
+
+class HomeProvider: NSObject {
+
+}
